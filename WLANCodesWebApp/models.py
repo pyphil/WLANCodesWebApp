ee7abce935cbbd1902d7e506b59828512f35abcd
pyphil <63059088+pyphil@users.noreply.github.com>
2022-09-09 20:54:17 +0200

    group

--- WLANCodesWebApp/models.py
+++ WLANCodesWebApp/models.py
@@ -14,6 +14,7 @@ class Code(models.Model):
 class Student(models.Model):
     name = models.CharField(max_length=50)
     firstname = models.CharField(max_length=50)
+    group = models.CharField(max_length=3)
     code = models.CharField(max_length=10)
 
 
