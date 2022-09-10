60e3a89c0218fc5da9882a729c84255df19ca157
pyphil <63059088+pyphil@users.noreply.github.com>
2022-09-10 12:19:20 +0200

    table

--- WLANCodesWebApp/urls.py
+++ WLANCodesWebApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.codes, name='codes'),
+    path('new_student/', views.new_student, name='new_student'),
     path('students/', views.students, name='students'),
 ]
--- WLANCodesWebApp/views.py
+++ WLANCodesWebApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Student
 from .forms import StudentForm
 
 
@@ -6,12 +7,18 @@ def codes(request):
     return render(request, 'codes.html', {'text': "12345-67891"})
 
 
-def students(request):
+def new_student(request):
     if request.method == 'GET':
         f = StudentForm()
-        return render(request, 'students.html', {'form': f})
+        return render(request, 'new_student.html', {'form': f})
     if request.method == 'POST':
         f = StudentForm(request.POST)
         if f.is_valid():
             f.save()
         return redirect('codes')
+
+
+def students(request):
+    students = Student.objects.all()
+    if request.method == 'GET':
+        return render(request, 'students.html', {'students': students})
