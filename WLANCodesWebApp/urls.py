ed403677e3d193934c50b118209e3396a858f5dc
pyphil <63059088+pyphil@users.noreply.github.com>
2022-09-10 16:48:20 +0200

    csv import

--- WLANCodesWebApp/urls.py
+++ WLANCodesWebApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.codes, name='codes'),
     path('new_student/', views.new_student, name='new_student'),
     path('students/', views.students, name='students'),
+    path('student_import/', views.student_import, name='student_import'),
 ]
--- WLANCodesWebApp/views.py
+++ WLANCodesWebApp/views.py
@@ -22,3 +22,23 @@ def students(request):
     students = Student.objects.all()
     if request.method == 'GET':
         return render(request, 'students.html', {'students': students})
+
+
+def student_import(request):
+    if request.method == 'GET':
+        return render(request, 'student_import.html', {})
+    if request.method == 'POST':
+        text = request.POST.get('input')
+        for line in text.split('\n'):
+            item = line.split(';')
+            if Student.objects.filter(email=item[3]):
+                pass
+            else:
+                # import
+                Student.objects.create(
+                    name=item[0],
+                    firstname=item[1],
+                    group=item[2],
+                    email=item[3]
+                )
+        return redirect('codes')
