b858df6bbb4bc021ae603c4dff6500e6f7fb87af
pyphil <63059088+pyphil@users.noreply.github.com>
2023-09-19 18:22:51 +0200

    user profile settings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('email_check/', views.email_check, name='email_check'),
     path('registation_email/', views.registration_email, name='registration_email'),
     path('account_success/', views.account_success, name='account_success'),
+    path('change_user/', views.change_user, name='change_user'),
 ]
